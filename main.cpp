92456ea5b88c97b904745364b8bed2f8f646bcb9
dfp <1066695510@qq.com>
2023-09-27 16:44:04 +0800

    It's a test!

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by dfp on 2023/9/27.
+//
+#include <iostream>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    cout << "It's just a test!" << endl;
+    return 0;
+}
